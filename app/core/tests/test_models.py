8010542ddff29be5c2d9ceb717c08dbbc077126b
JONO <jonathan88n@gmail.com>
2021-03-23 18:03:19 -0400

    Added custom user model

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -30,7 +30,7 @@ class ModelTests(TestCase):
 
     def test_create_new_superuser(self):
         """Test creating a new superuser"""
-        user = get_user_model().objects.create_user(
+        user = get_user_model().objects.create_superuser(
             'test@email.com',
             'test123',
         )
